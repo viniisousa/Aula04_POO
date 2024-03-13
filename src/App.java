c45071178b06985d9b8be8544da003420d9d931d
Vinicius Sousa <viniciussousapereiravini@gmail.com>
2024-03-13 12:40:55 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        Aluno viny = new Aluno("Viny","555.222.888-40",22225241,"01/08/2002","Masculino");
+        
+        System.out.println(viny.getNome());
+        System.out.println(viny.getRa());
+    }
+}
